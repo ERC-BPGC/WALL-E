9ae3e817450482db38e5ff3ed7ff359cb02fe303
Aadit Singla <146558658+aadit101@users.noreply.github.com>
2024-02-07 23:43:16 +0530

    Add files via upload

---
+++ dc_motor_cmds.ino
@@ -0,0 +1,93 @@
+int dir2_left=2;
+int dir1_left=3;
+int dir1_right=4;
+int dir2_right=5;
+int speedPinLeft = 10;
+int speedPinRight = 11;
+int mSpeed = 255;  // max speed
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(dir1_left, OUTPUT);
+  pinMode(dir2_left, OUTPUT);
+  pinMode(speedPinLeft, OUTPUT);
+  pinMode(dir1_right, OUTPUT);
+  pinMode(dir2_right, OUTPUT);
+  pinMode(speedPinRight, OUTPUT);
+
+
+  Serial.begin(9600);
+
+}
+
+void forward()
+{
+  digitalWrite(dir1_left, LOW);
+  digitalWrite(dir2_left, HIGH);
+  analogWrite(speedPinLeft, mSpeed);
+  digitalWrite(dir1_right, LOW);
+  digitalWrite(dir2_right, HIGH);
+  analogWrite(speedPinRight, mSpeed);
+}
+
+void right()
+{
+  digitalWrite(dir1_left, HIGH);
+  digitalWrite(dir2_left, LOW);
+  analogWrite(speedPinLeft, mSpeed);
+  digitalWrite(dir1_right, LOW);
+  digitalWrite(dir2_right, HIGH);
+  analogWrite(speedPinRight, mSpeed);
+}
+
+void left()
+{
+  digitalWrite(dir1_left, LOW);
+  digitalWrite(dir2_left, HIGH);
+  analogWrite(speedPinLeft, mSpeed);
+  digitalWrite(dir1_right, HIGH);
+  digitalWrite(dir2_right, LOW);
+  analogWrite(speedPinRight, mSpeed);
+}
+
+void backward()
+{
+  digitalWrite(dir1_left, HIGH);
+  digitalWrite(dir2_left, LOW);
+  analogWrite(speedPinLeft, mSpeed);
+  digitalWrite(dir1_right, HIGH);
+  digitalWrite(dir2_right, LOW);
+  analogWrite(speedPinRight, mSpeed);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+void Happy{
+  servoLArm.write(90)
+  servoRArm.write(90)
+  servoLEye.write(45)
+  int posArm = 90;
+  for (
+}
+
+}
+
+}
+
+
+//function for another file, testing here
+void sweep2ServosForward(Servo servo1, int starter1, int ender1, Servo servo2, int starter2, int ender2){
+  for (int angle1 = starter1, int angle2 = starter2; angle1 <= ender1 || angle2 <= ender2; angle1 += 5, angle2 += 5){
+    if (angle1<=ender1)
+    servo1.write(angle1);
+    if (angle2<=ender2)
+    servo2.write(angle2);
+    delay(15);
+  }
+}
+
+
+
+
+
